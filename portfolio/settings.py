55aefd133ee42e5e6233dcf01a8769ba234b16c3
Cristian Spa. <cristian.c.spagnuolo@gmail.com>
2024-08-26 00:44:23 +0200

    Procfile update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'home.middleware.WWWRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
