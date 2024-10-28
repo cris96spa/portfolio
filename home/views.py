a146e1f19884c4e8fc7a92e1a6873205e5352c3b
Cristian C. Spagnuolo <cristian.c.spagnuolo@gmail.com>
2024-10-28 23:40:47 +0100

    save work

--- home/views.py
+++ home/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
-from home import models
-# Create your views here.
+from home.models import (
+    Project,
+    Category,
+    SkillCategory,
+    Skill,
+)
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 
-def home(request):
-    projects = models.Project.objects.order_by('rank', 'name')
-    categories = models.Category.objects.order_by('rank', 'name')
-    skill_categories = models.SkillCategory.objects.order_by('rank', 'name')
-    skills = models.Skill.objects.order_by('-level', )
-    return render(request, 'home.html', {'projects': projects, 'categories': categories, 'skill_categories': skill_categories, 'skills': skills})
 
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
-        contact = models.Contact(name=name, email=email, subject=subject, message=message)
-        contact.save()
-    return render(request, 'contact.html')
+def home(request: HttpRequest) -> HttpResponse:
+    projects = Project.objects.order_by('rank', 'name')
+    categories = Category.objects.order_by('rank', 'name')
+    skill_categories = SkillCategory.objects.order_by('rank', 'name')
+    skills = Skill.objects.order_by(
+        '-level',
+    )
+    return render(
+        request,
+        'home.html',
+        {
+            'projects': projects,
+            'categories': categories,
+            'skill_categories': skill_categories,
+            'skills': skills,
+        },
+    )
